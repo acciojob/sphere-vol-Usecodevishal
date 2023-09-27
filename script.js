807512dd9b94f31fc9ae48d9a4d5ae73db78fd95
Vishal Bharti <100204338+Usecodevishal@users.noreply.github.com>
2023-09-27 19:53:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault();
+  let submit = document.addEventListener("click",function(e){
+	  e.preventDefault();
+	  let r = document.getElementById("radius").value;
+	  let capacity = (4/3)*Math.PI*r*r*r;
+	  let displayVol = document.getElementById("volume").value = capacity;
+	  
+  })
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
